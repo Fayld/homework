dcb47cb9e0a451aede542a33da272ec74cbbfea7
Алексей Волков <aleks3204932@mail.ru>
2023-10-08 12:09:45 +0300

    Initial commit

---
+++ hello.pas
@@ -0,0 +1,12 @@
+﻿begin
+  var s:=readinteger('Ввод цифры (языка),на котором говорит пользователь 1-русский ,2-английский, 3-немецкий 4-французский : ');
+  assert((s>=1)and (s<=4));
+  if s=1 then
+    println('Привет')
+  else if s=2 then
+    println('Hello')
+  else if s=3 then
+    println('Hallo')
+  else
+    println('Salut');
+end.
